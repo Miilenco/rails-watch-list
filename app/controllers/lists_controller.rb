445d3471dedf30fe9c6cff0e7884b2b2de77e5a8
Milenco Ieremici <milenco@widgit.com>
2025-02-27 14:26:43 +0000

    Added 'new' and 'create' to the 'Lists' controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,23 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
+  def new
+    @new_list = List.new
+  end
+
+  def create
+    @new_list = List.new(list_params)
+    if @new_list.save
+      redirect_to list_path(@new_list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
