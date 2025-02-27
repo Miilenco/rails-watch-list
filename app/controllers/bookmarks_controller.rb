f3d7060b02cbe0c0b312341e472b034e2bf0b465
Milenco Ieremici <milenco@widgit.com>
2025-02-27 14:48:35 +0000

    Added 'new' and 'create' to the 'Bookmarks' controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,26 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+  def new
+    @new_bookmark = Bookmark.new
+  end
+
+  def create
+    @new_bookmark = Bookmark.new(bookmark_params)
+    @new_bookmark.list = @list
+    if @new_bookmark.save
+      redirect_to list_path(@new_bookmark.list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
