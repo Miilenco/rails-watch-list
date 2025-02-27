4162e36324192d81b5376316c6e8e95f3c1c80d5
Milenco Ieremici <milenco@widgit.com>
2025-02-27 13:04:00 +0000

    Updated the seed to contain lists and bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,36 +14,60 @@ Bookmark.destroy_all
 Movie.destroy_all
 
 puts 'Adding movies...'
-Movie.create!(
+wonder_woman_movie = Movie.create!(
   title: "Wonder Woman 1984",
   overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
   poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
   rating: 6.9
 )
-puts 'Added Wonder Woman 1984'
+puts 'Added Wonder Woman 1984 movie'
 
-Movie.create(
+shawshank_redemption_movie = Movie.create!(
   title: "The Shawshank Redemption",
   overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
   poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
   rating: 8.7
 )
-puts 'Added The Shawshank Redemption'
+puts 'Added The Shawshank Redemption movie'
 
-Movie.create(
+titanic_movie = Movie.create!(
   title: "Titanic",
   overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
   poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
   rating: 7.9
 )
-puts 'Added Titanic'
+puts 'Added Titanic movie'
 
-Movie.create(
+oceans_eight_movie = Movie.create!(
   title: "Ocean's Eight",
   overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
   poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
   rating: 7.0
 )
-puts "Added Ocean's Eight"
+puts "Added Ocean's Eight movie"
 
 puts "#{Movie.count} movies have been added."
+
+puts "Adding lists..."
+favourites_list = List.create!(name: "Favourites")
+puts "Added Favourites list"
+
+recommendations_list = List.create!(name: "Recommendations")
+puts "Added Recommendations list"
+
+puts "#{List.count} lists have been added"
+
+puts "Adding movies to lists..."
+add_wonder_woman_to_favourites = Bookmark.new(comment: "Best movie ever!")
+add_wonder_woman_to_favourites.movie = wonder_woman_movie
+add_wonder_woman_to_favourites.list = favourites_list
+add_wonder_woman_to_favourites.save!
+puts "Added Wonder Woman 1984 to Favourites"
+
+add_titanic_to_recommendations = Bookmark.new(comment: "My partner's recommendation")
+add_titanic_to_recommendations.movie = titanic_movie
+add_titanic_to_recommendations.list = recommendations_list
+add_titanic_to_recommendations.save!
+puts "Added Titanic to Recommendations"
+
+puts "#{Bookmark.count} bookmarks have been added"
